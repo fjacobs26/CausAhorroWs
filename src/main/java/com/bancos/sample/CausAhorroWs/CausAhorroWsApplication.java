1141bf6fb821b0680f137722c95cc3215469114c
Felipe Manuel Jacobs Osorio <felipejacobs28@gmail.com>
2018-09-19 00:18:03 +0200

    Snapshot 0.0.1
    Add needed repositories

---
+++ src/main/java/com/bancos/sample/CausAhorroWs/CausAhorroWsApplication.java
@@ -0,0 +1,12 @@
+package com.bancos.sample.CausAhorroWs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CausAhorroWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CausAhorroWsApplication.class, args);
+	}
+}
