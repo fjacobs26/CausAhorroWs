9d1c13bc6fa97aba74a0b9bd91ff2a9b22d09e1c
Felipe Manuel Jacobs Osorio <felipejacobs28@gmail.com>
2018-09-22 16:11:52 +0200

    Snapshot1.0.0
    Cambio de la logica de datos random, se pone unos ejemplos en formato json

---
+++ src/test/java/com/bancos/sample/CausAhorroWs/RandomTest.java
@@ -0,0 +1,10 @@
+package com.bancos.sample.CausAhorroWs;
+
+import com.bancos.sample.CausAhorroWs.Model.bank.Account;
+
+public class RandomTest {
+	Account acount = null;
+	public static void main(String[] args) {
+		
+	}
+}
